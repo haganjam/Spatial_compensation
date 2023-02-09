c3c4af37766071ef857117ece5cd2958fd66c496
James Hagan <james_hagan@outlook.com>
2023-02-09 13:53:18 +0100

    update the analysis and remake the figures with the new growth rate model analysis

--- scripts/05_compensation_analysis.R
+++ scripts/05_compensation_analysis.R
@@ -24,8 +24,8 @@ sp_dat <- tibble(sp_dat)
 # calculate the percent deviation from the initial productivity
 sp_dat <- 
   sp_dat %>%
-  mutate(thresh_50 = (prod_comp >= 0.50*prod_init),
-         prod_change = ((prod_comp - prod_init)/prod_init)*100  ) %>%
+  mutate(prod_change = (prod_comp - prod_init)) %>%
+  mutate(prod_change_perc = (prod_change/prod_init)*100) %>%
   mutate(depth = as.character(depth),
          comp_level = as.character(comp_level))
 
@@ -34,7 +34,7 @@ summary(sp_dat)
 
 # check the maxima: These marginal cases
 sp_dat %>%
-  filter(prod_change > 10000)
+  filter(prod_change_perc > 10000)
 
 # modify the depth factor
 # change the factor levels of depth
@@ -44,8 +44,8 @@ levels(sp_dat$depth) <- c("-2 to -14", "-14 to -26", "-26 to -38", "-38 to -50")
 # remove the most extreme values
 sp_dat <- 
   sp_dat %>%
-  filter(prod_change > quantile(prod_change, 0.01),
-         prod_change < quantile(prod_change, 0.99))
+  filter(prod_change_perc > quantile(prod_change_perc, 0.01),
+         prod_change_perc < quantile(prod_change_perc, 0.99))
 
 # check the summary data
 summary(sp_dat)
@@ -54,25 +54,30 @@ summary(sp_dat)
 sp_sub <- 
   sp_dat %>%
   group_by(species_ext, depth, comp_level) %>%
-  mutate(sd = sd(prod_change)) %>%
   sample_n(size = 100) %>%
   filter(comp_level %in% c(0.1, 0.5, 0.9)) %>%
-  filter(sd != 0)
+  filter(prod_change != 0)
 summary(sp_sub)
 
 # summarise the data
 sp_sum <- 
   sp_dat %>%
   group_by(species_ext, depth, comp_level) %>%
-  summarise(mu = mean(prod_change),
-            PI_low = quantile(prod_change, 0.05),
-            PI_high = quantile(prod_change, 0.95),
+  summarise(mu = mean(prod_change_perc),
+            PI_low = quantile(prod_change_perc, 0.05),
+            PI_high = quantile(prod_change_perc, 0.95),
             n = n(), .groups = "drop") %>%
   filter(comp_level %in% c(0.1, 0.5, 0.9))
 
 # plot these four graphs individually for each species
 sp_code <- c("fu_sp", "fu_ve", "as_no", "fu_se")
 sp_names <- c("F. spiralis Extinct", "F. vesiculosus Extinct", "A. nodosum Extinct", "F. serratus Extinct")
+
+# axis labels
+ylabs <- c("Change in productivity (%)", "", "Change in productivity (%)", "")
+xlabs <- list(NULL, NULL, "Depth range (cm)", "Depth range (cm)")
+x.text <- c("white", "white", "black", "black")
+x.text.size <- c(1, 1, 10, 10)
 
 plots <- vector("list", length = length(sp_code))
 for(i in 1:length(sp_code)) {
@@ -82,39 +87,38 @@ for(i in 1:length(sp_code)) {
     geom_hline(yintercept = 0, linetype = "dashed") +
     geom_hline(yintercept = 50, linetype = "dashed", colour = "red") +
     geom_hline(yintercept = -50, linetype = "dashed", colour = "red") +
+    geom_errorbar(data = sp_sum %>% filter(species_ext == sp_code[i]),
+                  mapping = aes(x = depth, colour = comp_level,
+                                ymin = PI_low, ymax = PI_high),
+                  width = 0,
+                  position = position_dodge(0.8),
+                  show.legend=FALSE) +
     geom_quasirandom(data = sp_sub %>% filter(species_ext == sp_code[i]),
-                     mapping = aes(x = depth, y = prod_change, colour = comp_level),
+                     mapping = aes(x = depth, y = prod_change_perc, colour = comp_level),
                      dodge.width = 0.8, alpha = 0.2, shape = 1, size = 2.25,
                      show.legend = FALSE) +
     geom_point(data = sp_sum %>% filter(species_ext == sp_code[i]),
                mapping = aes(x = depth, y = mu, colour = comp_level), 
                position = position_dodge(0.8), shape = 18, size = 3.5) +
-    geom_errorbar(data = sp_sum %>% filter(species_ext == sp_code[i]),
-                  mapping = aes(x = depth, colour = comp_level,
-                                ymin = PI_low, ymax = PI_high),
-                  width = 0.05,
-                  position = position_dodge(0.8),
-                  show.legend=FALSE) +
-    scale_colour_viridis_d(option = "E", end = 0.9) +
-    ylab("Change in productivity (%)") +
-    xlab("Depth zone") +
+    scale_colour_viridis_d(option = "C", end = 0.9) +
+    ylab(ylabs[i]) +
+    xlab(xlabs[[i]]) +
     ggtitle(label = sp_names[i]) +
     labs(colour = "Compensation (%)") +
     guides(colour = guide_legend(override.aes = list(shape = 18, size = 6))) +
-    scale_y_continuous(limits = c(-370, 360)) +
+    scale_y_continuous(limits = c(-370, 370)) +
     theme(legend.key = element_rect(fill = NA),
           legend.position = "top") +
     theme_meta() +
     theme(legend.title = element_text(size = 12),
           legend.text = element_text(size = 11.5),
-          plot.title = element_text(size = 12, hjust = 0.5))
+          plot.title = element_text(size = 12, hjust = 0.5),
+          axis.text.x = element_text(colour = x.text[i], size = x.text.size[i]))
   
   # add to list
   plots[[i]] <- px
   
 }
-
-plots[[1]]
 
 # join these graphs into a single graph
 p1 <- 
@@ -122,10 +126,11 @@ p1 <-
             nrow = 2, ncol = 2,
             common.legend = TRUE, legend = "bottom",
             labels = c("a", "b", "c", "d"),
-            font.label = list(face = "plain", size = 11))
+            font.label = list(face = "plain", size = 11),
+            heights = c(1, 1.2))
 plot(p1)
 
-ggsave(filename = "figures/fig4.png", p1, dpi = 400,
+ggsave(filename = here("figures/fig4.png"), p1, dpi = 400,
        units = "cm", width = 18, height = 17)
 
 ### END
