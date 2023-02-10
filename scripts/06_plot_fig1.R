c6367a500c161601e84c1b65b7b566a0952acb93
James Hagan <james_hagan@outlook.com>
2023-02-10 10:44:43 +0100

    update the figure 1 plotting

--- scripts/06_plot_fig1.R
+++ scripts/06_plot_fig1.R
@@ -7,36 +7,37 @@ library(readr)
 library(tidyr)
 library(ggplot2)
 library(ggpubr)
-
-# set the working directory
-setwd("C:/Users/james/OneDrive/PhD_Gothenburg/Chapter_2_Fucus_landscape/compensation_analysis")
-getwd()
+library(here)
 
 # load the plotting theme
-source("scripts/function_plotting_theme.R")
+source(here("scripts/function_plotting_theme.R"))
 
-p1 <- readRDS(file = "figures/Fig_1a.rds")
+p1 <- readRDS(file = here("figures/fig1a.rds"))
 p1 <- 
   p1 + 
-  ggtitle("")
-p2 <- readRDS(file = "figures/Fig_1b.rds")
+  ggtitle("") +
+  theme(legend.position = "none")
+
+p2 <- readRDS(file = here("figures/fig1b.rds"))
 p2 <- 
   p2 + 
-  ggtitle("")
-p3 <- readRDS(file = "figures/Fig_1c.rds")
+  ggtitle("") +
+  theme(legend.position = "none")
+
+p3 <- readRDS(file = here("figures/fig1c.rds"))
 p3 <- 
   p3 + 
-  ggtitle("")
+  ggtitle("") +
+  theme(legend.position = "none")
 
 # ggarrange the plots to keep the common legend
-p123 <- ggarrange(p1, p2, p3, common.legend = TRUE, legend = "top",
+p123 <- ggarrange(p1, p2, p3, common.legend = FALSE,
                  labels = c("a", "b", "c"), font.label = list(face = "plain", size = 11),
-                 ncol = 3, nrow = 1,
-                 widths = c(1, 1.1, 1))
+                 ncol = 2, nrow = 2,
+                 widths = c(1, 1, 1))
 plot(p123)
 
 ggsave(filename = "figures/fig1.png", p123, dpi = 400,
-       units = "cm", width = 21, height = 10)
+       units = "cm", width = 20, height = 17)
 
 ### END
-
