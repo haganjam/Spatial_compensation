3b5fd00a1de294822bf4b86b068e23400d5927f1
James Hagan <james_hagan@outlook.com>
2023-03-21 11:43:38 +0100

    add a file to create the description file

---
+++ code/create_description_file.R
@@ -0,0 +1,11 @@
+
+# description file code
+
+# load the holepunch library
+library(holepunch)
+
+# write the description file
+write_compendium_description(
+  package = "Spatial compensation",
+  description = "Spatial compensation of biomass production in Fucoid macroalgae",
+  version = "1.0")
