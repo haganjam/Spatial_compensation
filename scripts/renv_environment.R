94399fadbb89eeff88d96c2c6943c6f194fec514
James Hagan <james_hagan@outlook.com>
2023-03-21 10:11:42 +0100

    add the renv lockfile to the repository

---
+++ scripts/renv_environment.R
@@ -0,0 +1,21 @@
+#' @title - set-up a reproducible package environment using renv
+#' 
+#' @description - this scripts records the code used to create a local package dependency
+#' structure using the renv package. The 'cmdstanr' and 'rethinking' packages were ignored
+#' because they are not on CRAN and do not work well with renv
+#' 
+#' @author: James Hagan (james_hagan(at)outlook.com)
+
+# view currently-ignored packaged
+renv::settings$ignored.packages()
+
+# ignore cmdstanr and the rethinking package because these didn't work with renv
+renv::settings$ignored.packages(c("cmdstanr", "rethinking"), persist = FALSE)
+
+# initialise the renv local library
+renv::init()
+
+# check the status
+renv::status()
+
+### END
