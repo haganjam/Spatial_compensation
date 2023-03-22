63d0f2a359ee86904489507a8468e84095cdc4ba
James Hagan <james_hagan@outlook.com>
2023-03-22 11:52:01 +0100

    add data and meta data along with updating the description file

--- code/helper-description-file.R
+++ code/helper-description-file.R
@@ -4,6 +4,16 @@
 # load the holepunch library
 library(holepunch)
 
+# set the author name etc.
+options(
+  usethis.full_name = "James Hagan",
+  usethis.description = list(
+    `Authors@R` = 'person("James", "Hagan", email = "james_hagan@outlook.com", role = c("aut", "cre"), 
+    comment = c(ORCID = "0000-0002-7504-3393"))',
+    License = "MIT",
+    Version = "1.0.0"
+  ))
+
 # write the description file
 write_compendium_description(
   package = "Spatial compensation",
